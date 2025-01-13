955ea4cbaaa37be20db03eafb44acfc8844efce3
DaytonKlein <daytonsk@hotmail.com>
2025-01-13 10:27:26 -0300

    Adicionei estilos
    Nesta atualização, eu adicionei um estilo novo para a calculadora, implementei um front end onde tem mais cores e vida para projeto.

--- calc.php
+++ calc.php
@@ -1,8 +1,24 @@
-<?php 
-$nome = $_GET["nome"];
-$hora = $_GET["hora"];
-$salario = 22 * 9 * $hora;
+<!DOCTYPE html>
+<html lang="pt-br">
 
-echo "Este é seu salario R$ $salario, aproveite com moderação $nome!";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-?>
+<body>
+
+    <?php
+    $nome = $_GET["nome"];
+    $hora = $_GET["hora"];
+    $salario = 22 * 9 * $hora;
+
+    echo "Este é seu salario R$ $salario, aproveite com moderação $nome!";
+
+    ?>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,16 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de Salario</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <section>
     <h1>Calculadora de Salario</h1>
     <form action="calc.php">
         <label for="">Digite seu Nome:</label><br>
         <input type="text" name="nome"><br><br>
         <label for="">Digite o Valor da HORA:</label><br>
         <input type="number" name="hora" id=""><br><br>
-        <input type="submit" value="enviar">
+        <input type="submit" value="Enviar">
     </form>
+    </section>
 </body>
 
 </html>
